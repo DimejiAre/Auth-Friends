1935b0d2388ebfb1ceb0d9dd8130bbe97a77526d
DimejiAre <olardi2@gmail.com>
2019-10-15 13:40:14 +0100

    add logout link

--- friends/src/components/NavBar.jsx
+++ friends/src/components/NavBar.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function NavBar(){
+function NavBar(props){
+
+    const logout = e => {
+        localStorage.removeItem('token');
+
+        props.history.replace('/');
+    }
+
     return (
         <nav className='navbar'>
             <h2>Friends</h2>
             <Link to='/'>Login</Link>
             <Link to='/friends'>Friends</Link>
+            <Link to='/' onClick={logout}>Logout</Link>
         </nav>
     )
 }
